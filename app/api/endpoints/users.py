7686fe5476af2b7f13427c8f5278666c6a6cc63e
David-Roklem <roklem@mail.ru>
2023-11-24 16:37:52 +0300

    Implement login view

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -12,7 +12,17 @@ users = {}
 def register(user: User):
     users['username'] = user.username
     users['password'] = user.password
-    return {"message": "The user was successfully registered"}
+    print(users)
+    return {'message': 'The user was successfully registered'}
+
+
+@app.post('/auth/login/')
+def login(user: User):
+    if user.username == users['username'] and\
+            user.password == users['password']:
+        return {'message': 'The user was successfully logged in'}
+    else:
+        return {'message': 'The user was not logged in'}
 
 
 if __name__ == '__main__':
