7d51032e93bee0c894f365b41c2efbcd1a4897ed
David-Roklem <roklem@mail.ru>
2023-11-29 11:34:14 +0300

    feat. Add psw hashing to view func
    Previously I forgot to check if the username provided
    by a new user already exists in the DB. Now it's fixed.
    Also, password hashing was added to the registration handler

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 import uvicorn
 
 from app.api.schemas.user import User
+from app.core.hash_password import get_password_hash, verify_password
 
 app = FastAPI()
 
@@ -9,15 +10,26 @@ users = {}
 
 
 @app.post('/auth/register/')
-def register(user: User):
+def register(user: User) -> dict:
+    # First of all I need to make sure that there no user with the same
+    # username as provided in my 'DB'
+    user_exists = users.get(user.username)
+    if user_exists:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='The user already exists in the DB'
+        )
+
+    # The next step is to hash the password provided
+    hashed_password = get_password_hash(user.password)
+
     users['username'] = user.username
-    users['password'] = user.password
-    print(users)
+    users['password'] = hashed_password
     return {'message': 'The user was successfully registered'}
 
 
 @app.post('/auth/login/')
-def login(user: User):
+def login(user: User) -> dict:
     if user.username == users['username'] and\
             user.password == users['password']:
         return {'message': 'The user was successfully logged in'}
