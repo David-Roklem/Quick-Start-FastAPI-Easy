26c5c14b4c74ace728af2f2c75999ae93ca51e41
David-Roklem <roklem@mail.ru>
2023-11-29 14:24:04 +0300

    fix. Yet another fix in Settings class
    It turned out that the Settings class implementatio was
    missing the variables from .env. So, as far as I understood,
    one should provide the exact names of these variables (from .env)
    to Settings class as its attributes. One could (and probably should)
    validate them with type annotations. And with model_config at place
    from that point those vars can be called at any place of the
    codebase with smth like:
    ```settings = Settings()
    settings.SECRET_KEY```

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
 
-    model_config = SettingsConfigDict(env_file=".env")
+    SECRET_KEY: str | None = None
+    ALGORITHM: str | None = None
+    ACCESS_TOKEN_EXPIRE_MINUTES: int | None = None
+
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
 
 settings = Settings()
